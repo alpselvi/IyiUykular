bac40831d9ace448d67ca0b6adbb039c5b0274f3
Alparslan Buğra Selvi <alparslan.bugra@hotmail.com>
2023-09-10 17:05:03 +0300

    Initial Commit

---
+++ IyiUykular/IyiUykularApp.swift
@@ -0,0 +1,17 @@
+//
+//  IyiUykularApp.swift
+//  IyiUykular
+//
+//  Created by Alp Selvi on 10.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct IyiUykularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
